68d6975abc5e4ebfc5e357167563dbaa53592edf
Daylight-Cloud <2738452268@qq.com>
2025-11-29 17:14:52 +0800

    解决冲突

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/', // ⚡ 关键：所有静态资源使用绝对路径 /assets/...
   plugins: [vue()],
-  build: {
-    outDir: 'dist', // 输出目录
-    assetsDir: 'assets', // 静态资源目录
-    rollupOptions: {
-      output: {
-        // JS/CSS 文件名带 hash 保证缓存更新
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
+  base: '/library/', // ⚠ 注意末尾 /
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://101.42.169.71:3000',
+        changeOrigin: true
       }
     }
   }
