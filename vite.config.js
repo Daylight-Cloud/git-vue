bcdbbfda61524fe21cd2c682ba887526328685b8
Daylight-Cloud <2738452268@qq.com>
2025-11-29 16:56:02 +0800

    保存本地更改

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/', // ⚡ 关键：所有静态资源使用绝对路径 /assets/...
   plugins: [vue()],
-  base: '/library',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://101.42.169.71:3000',
-        changeOrigin: true ,
-        // 保留 /api 前缀，因为后端路由都是 /api/*
+  build: {
+    outDir: 'dist', // 输出目录
+    assetsDir: 'assets', // 静态资源目录
+    rollupOptions: {
+      output: {
+        // JS/CSS 文件名带 hash 保证缓存更新
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
   }
